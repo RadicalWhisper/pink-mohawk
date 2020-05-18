2c2b87d19269ca3f59ddd31829de270674d0a6fd
John Thomas <jt@radicalwhisper.com>
2020-05-18 11:30:47 -0400

    Added legal notice

--- pinkmohawk.py
+++ pinkmohawk.py
@@ -182,4 +182,18 @@ async def adp(ctx):
     await ctx.send("Hits: **" + str(get_hits(savedPool)) + "** " + str(savedPool))
     await ctx.message.delete()
 
+@bot.command()
+async def legal(ctx):
+    await ctx.send("""The Topps Company, Inc. has sole ownership of the  
+                    names, logo, artwork, marks, photographs, sounds, 
+                    audio, video and/or any proprietary material used in
+                     connection with the game Shadowrun. The Topps 
+                     Company, Inc. has granted permission to Pink Mohawk
+                      to use such names, logos, artwork, marks and/or 
+                      any proprietary materials for promotional and 
+                      informational purposes on its website but does not
+                       endorse, and is not affiliated with Pink Mohawk 
+                       in any official capacity whatsoever.""")
+    await ctx.message.delete()
+
 bot.run(TOKEN)
