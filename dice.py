e030cdd1b653039aad85940b3015340f1c912b25
John Thomas <jt@radicalwhisper.com>
2020-05-18 15:23:33 -0400

    refactor dice.py

--- dice.py
+++ dice.py
@@ -4,94 +4,62 @@ Created on 16 May 2020
 """
 import random
 
-def rollPool(dice, options=None):
+def rollPool(dice, *args):
     pool = []
-    modifiers = []
-    glitches = 0
     hits = 0
     wild = False
     explode = False
-    twosGlitch = False
-    fivesCancelled = False
-    wildResult = []
-    modifiers = []
-    if options is not None:
-        modifiers = list(options)
-
-    if "w" in modifiers:
+    wild_val = 0
+    twos_glitch = False
+    explosions = 0
+    if "w" in args:
         wild = True
-    if "e" in modifiers:
+    if "e" in args:
         explode = True
-    if "2" in modifiers:
-        twosGlitch = True
+    if "!" in args:
+        twos_glitch = True
 
     for d in range(dice if wild == False else dice - 1):
         val = random.randint(1, 6)
-        if val >= 5:
-            hits += 1
-        if val == 1:
-            glitches += 1
-        if twosGlitch & val == 2:
-            glitches += 1
         pool.append(val)
         if explode:
             while val == 6:
+                explosions += 1
                 val = random.randint(1, 6)
-                if val >= 5:
-                    hits += 1
-                if val == 1:
-                    glitches += 1
                 pool.append(val)
     if wild:
-        val = random.randint(1, 6)
-        
-        if val >= 5:
-            hits += 3
-        if val == 1:
-            glitches += 1
-            fivesCancelled = True
-        if twosGlitch & val == 2:
-            glitches += 1
-        wildResult.append(val)
-        if explode:
-            while val == 6:
-                val = random.randint(1, 6)
-                if val >= 5:
-                    hits += 1
-                if val == 1:
-                    glitches += 1
-                wildResult.append(val)
-    if fivesCancelled:
-        for d in range(pool):
-            if pool[d] == 5:
-                hits -= 1
-    print("Pool: " + str(pool) + " Wild: " + str(wildResult) + " Hits: "
-         + str(hits) + " Glitches: " + str(glitches))
-    return pool, wildResult, hits, glitches
-
+        wild_val = random.randint(1, 6)
+      
+    pool.sort()
+    hits = get_hits(pool, wild_val)
+    print("Pool: " + str(pool) + " Wild: " + str(wild_val) + " Hits: "
+         + str(hits) + " Explosions: " + str(explosions) + " Glitch: " + str(is_glitch(pool)))
+    return pool, wild_val, hits, is_glitch(pool, "!" if twos_glitch else None)
 
 def get_hits(pool, wild = 0):
     hits = 0
-    for d in range(pool):
+    for d in range(len(pool)):
         if pool[d] >= 5:
             hits += 1
     if wild >= 5:
         hits += 3
     return hits
 
-def check_glitch(pool, wild = 0):
+def is_glitch(pool, *args):
     glitches = 0
-    for d in range(pool):
+    for d in range(len(pool)):
         if pool[d] == 1:
             glitches += 1
+        if "!" in args and pool[d] == 2:
+            glitches += 1
+    print("Glitches: " + str(glitches))
     if glitches > len(pool) / 2:
         return True
     else:
         return False
 
-
-
-# rollPool(3, "2ew")
+#rollPool(17, "e", "!", "w")
+#s_glitch([1,2,2,3,4,5], "!")
 
             
 
