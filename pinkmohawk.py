56e135c5afbb9654be235f98df6268ea187d6dfd
John Thomas <jt@radicalwhisper.com>
2020-05-18 11:35:09 -0400

    Update pinkmohawk.py

--- pinkmohawk.py
+++ pinkmohawk.py
@@ -184,16 +184,7 @@ async def adp(ctx):
 
 @bot.command()
 async def legal(ctx):
-    await ctx.send("""The Topps Company, Inc. has sole ownership of the  
-                    names, logo, artwork, marks, photographs, sounds, 
-                    audio, video and/or any proprietary material used in
-                     connection with the game Shadowrun. The Topps 
-                     Company, Inc. has granted permission to Pink Mohawk
-                      to use such names, logos, artwork, marks and/or 
-                      any proprietary materials for promotional and 
-                      informational purposes on its website but does not
-                       endorse, and is not affiliated with Pink Mohawk 
-                       in any official capacity whatsoever.""")
+    await ctx.send("The Topps Company, Inc. has sole ownership of the names, logo, artwork, marks, photographs, sounds, audio, video and/or any proprietary material used in connection with the game Shadowrun. The Topps Company, Inc. has granted permission to Pink Mohawk to use such names, logos, artwork, marks and/or any proprietary materials for promotional and informational purposes on its website but does not endorse, and is not affiliated with Pink Mohawk in any official capacity whatsoever.")
     await ctx.message.delete()
 
 bot.run(TOKEN)
