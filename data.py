8dd22a7f8da69a4678fac0d6c9e0ab98ceba3801
John Thomas <jt@radicalwhisper.com>
2020-05-22 12:17:40 -0400

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate('firebase-service-account.json')
-firebase_admin.initialize_app(cred)
+#cred = credentials.Certificate('firebase-service-account.json')
+firebase_admin.initialize_app()
 
 DB = firestore.client()
